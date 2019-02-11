a197d23325d85e86d1f9339362cf8264edd855cb
xernyuii <hfutxc.xern@gmail.com>
2019-02-11 21:18:19 +0800

    add config & record support

--- CleanToolAdmin.py
+++ CleanToolAdmin.py
@@ -1,6 +1,8 @@
-import sys, getopt
+import sys
+import getopt
 import os
 import json
+import time
 
 # Usage: CleanToolAdmin.py --mode=protect/force/record/clean --config=conf.json --debug --help
 
@@ -13,6 +15,7 @@ def main():
     parsed_config = ""
     mode = ""
 
+    # parse arguments
     try:
         opts, args = getopt.getopt(sys.argv[1:], "m:c:dh",["mode=", "config=", "debug", "help"])
         print("~",opts)
@@ -24,9 +27,10 @@ def main():
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             print("#help")
+            help()
             sys.exit()
         elif opt in ("-m", "--mode"):
-            inputfile = arg
+            mode = arg
             print("#mode")
         elif opt in ("-c", "--config"):
             config_file = arg
@@ -46,6 +50,34 @@ def main():
     elif mode == "":
         print("ERROR: Mode not selected")
         sys.exit(2)
+
+    # config file arguments
+    search_type = parsed_config["search_type"]
+    set_time = parsed_config["set_time"]
+    set_direction = parsed_config["set_direction"]
+    record_file = parsed_config["record_file"]
+    parsed_record_file = {}
+
+    now = time.time()
+    if os.path.exists(record_file):
+        with open(record_file, 'r') as recordfile:
+            parsed_record_file = json.load(recordfile)
+        parsed_record_file["last_modified_date"]=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    else:
+        parsed_record_file = {"last_modified_date" : time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), "files_on_mark" : []}
+    
+    for path, dirs, files in os.walk(set_direction):
+        for name in files:
+            fullpath = os.path.join(path, name)
+            if os.path.exists(fullpath):
+                mtime = os.path.getmtime(fullpath)
+                # if (now - mtime) > set_time:
+                #     print(fullpath)
+                print(fullpath)
+                parsed_record_file["files_on_mark"].append({"state" : "mark", "dir" : fullpath, "search_type" : search_type, "time_res" : "1000", "size" : "100", "search_date" : "2019-1-1"})
+
+    with open(record_file,"w") as dump_f:
+        json.dump(parsed_record_file, dump_f, indent=4, separators=(',', ':'))
 
 if __name__ == "__main__":
     main()
